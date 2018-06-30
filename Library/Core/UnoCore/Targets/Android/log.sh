d07d4c3c8438062ff54848c0335986eca018a208
Morten Daniel Fornes <mdfornes@gmail.com>
2018-06-30 15:31:16 +0800

    Android: remove legacy log scripts
    These scripts are really old and no longer useful, I think.
    
    Let's get them out of the way.

--- Library/Core/UnoCore/Targets/Android/log.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# @(MSG_ORIGIN)
-# @(MSG_EDIT_WARNING)
-ADB_PATH="@(SDK.Directory)/platform-tools/adb"
-$ADB_PATH devices | head -n 2 | tail -n 1 | cut -sf 1 | xargs -I {} $ADB_PATH -s {} logcat -s "Debug","dalvik","error","Error","XliApp","Xli","DEBUG","System.err","BufferQueue","AndroidRuntime","LogSvc","Adreno-ES20","Adreno-EGL","libEGL","BDWGC","art","OpenGLRenderer","HardwareRenderer","@(Activity.Name)"
