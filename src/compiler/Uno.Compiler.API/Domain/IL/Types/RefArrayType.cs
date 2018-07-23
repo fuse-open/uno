eddb8569b83cc4fee14af70675de24974617f6f2
Morten Daniel Fornes <mdfornes@gmail.com>
2018-07-23 16:45:50 +0700

    Revert "Uno.Compiler: don't generate Length property on RefArrayType"
    This reverts commit d5c2e0f6f412f8c451459c02c7bb7caa7c4144ac.

--- src/compiler/Uno.Compiler.API/Domain/IL/Types/RefArrayType.cs
+++ src/compiler/Uno.Compiler.API/Domain/IL/Types/RefArrayType.cs
@@ -6,13 +6,21 @@ namespace Uno.Compiler.API.Domain.IL.Types
     {
         public static RefArrayType CreateMaster(DataType intType, DataType objectType)
         {
-            return new RefArrayType(objectType.Source, objectType, objectType);
+            var result = new RefArrayType(objectType.Source, objectType, objectType);
+            var length = new Property(objectType.Source, null, Modifiers.Public | Modifiers.Extern | Modifiers.Generated, "Length", result, intType);
+            length.CreateGetMethod(objectType.Source, length.Modifiers);
+            result.Properties.Add(length);
+            return result;
         }
 
         public static RefArrayType Create(RefArrayType master, DataType elmType)
         {
             var result = new RefArrayType(elmType.Source, elmType, master.Base);
             result.SetMasterDefinition(master);
+            var length = new Property(elmType.Source, null, Modifiers.Public | Modifiers.Extern | Modifiers.Generated, "Length", result, master.Properties[0].ReturnType);
+            length.CreateGetMethod(elmType.Source, length.Modifiers);
+            length.SetMasterDefinition(master.Properties[0]);
+            result.Properties.Add(length);
             return result;
         }
 
