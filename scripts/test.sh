2af2b3e1964d17184bc25318afcd262a2ef161e3
Morten Daniel Fornes <mdfornes@gmail.com>
2018-11-25 18:26:03 +0700

    run more tests on CI
    This fixes up test.sh and brings back UnoTest, UXTest and our old compiler
    tests, which are currently not being run.
    
    Both AppVeyor and Travis now run tests by invoking test.sh, which reduces
    duplication and seems to be cleaner than it was before.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+SELF=`echo $0 | sed 's/\\\\/\\//g'`
+cd "`dirname "$SELF"`/.." || exit 1
+source scripts/common.sh
 
-ROOT=`dirname $0`/..
-COMPILATION_DIR=/tmp/PackageCompilationTest
+# Arguments
+TARGET=$1
 
-"$ROOT/bin/uno" test lib $*
-"$ROOT/bin/uno" test-gen "$ROOT/lib" "$COMPILATION_DIR"
-"$ROOT/bin/uno" build --target=cmake --no-strip --clean "$COMPILATION_DIR"
+# Run uno tests
+uno test $TARGET lib
+uno test $TARGET tests/src/{Uno,UX}Test
+
+# Run compiler tests
+function uno-compiler-test {
+    for config in Debug Release; do
+        exe=src/testing/Uno.CompilerTestRunner/bin/$config/uno-compiler-test.exe
+        if [ -f $exe ]; then
+            dotnet-clr $exe
+            return $?
+        fi
+    done
+
+    echo "ERROR: uno-compiler-test.exe was not found"
+    return 1
+}
+
+uno-compiler-test
+
+# Check that all packages build without errors
+function packages-build-test {
+    dir=.test/build-$1
+    uno test-gen $1 $dir
+    uno build $TARGET --no-strip $dir
+}
+
+packages-build-test lib
