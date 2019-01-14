55c8aadc5dd9695a6621fa8b13981b5f69abc7f4
Morten Daniel Fornes <mdfornes@gmail.com>
2019-01-14 08:07:29 +0700

    add build target for metadata
    Example:
    
        uno build metadata --out-dir .uno/metadata
    
    This will produce reference assemblies (one for each package) that contain
    metadata suitable for code-completion and refactoring plugins, saved to the
    specified location.

---
+++ src/compiler/Uno.Compiler.Backends.Metadata/ErrorCode.cs
@@ -0,0 +1,8 @@
+﻿namespace Uno.Compiler.Backends.Metadata
+{
+    enum ErrorCode
+    {
+        E0000,
+        I0000,
+    }
+}
---
+++ src/compiler/Uno.Compiler.Backends.Metadata/MetadataBackend.cs
@@ -0,0 +1,55 @@
+﻿using Uno.Compiler.Backends.CIL;
+using Uno.Compiler.API.Backends;
+using Uno.Compiler.API.Domain.IL;
+
+namespace Uno.Compiler.Backends.Metadata
+{
+    public class MetadataBackend : Backend
+    {
+        CilLinker _linker;
+
+        public override string Name => "Meta";
+
+        public MetadataBackend(ShaderBackend shaderBackend)
+            : base(shaderBackend)
+        {
+        }
+
+        public override void Configure()
+        {
+            _linker = new CilLinker(Log, Essentials, true);
+        }
+
+        public override bool CanLink(SourcePackage upk)
+        {
+            return Environment.IsUpToDate(upk, upk.Name + ".dll");
+        }
+
+        public override bool CanLink(DataType dt)
+        {
+            return dt.Package.CanLink;
+        }
+
+        public override bool CanLink(Function function)
+        {
+            return true;
+        }
+
+        public override BackendResult Build(SourcePackage package)
+        {
+            if (package.CanLink)
+            {
+                package.Tag = _linker.AddAssemblyFile(Environment.Combine(package.Name + ".dll"));
+                return null;
+            }
+
+            var g = new MetadataGenerator(Disk, Data, Essentials,
+                                          this, _linker, package,
+                                          Environment.OutputDirectory);
+            g.Configure();
+            g.Generate();
+            g.Save();
+            return null;
+        }
+    }
+}
