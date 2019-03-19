76ecbeb8b4e53a299766a79b88d583086c0f885e
Morten Daniel Fornes <mdfornes@gmail.com>
2019-03-20 04:43:16 +0700

    Android: suggest installing android-build-tools
    When the SDK or NDK aren't found during build we will now suggest an easy
    way to fix the problem.

--- lib/UnoCore/Targets/Android/build.bat
+++ lib/UnoCore/Targets/Android/build.bat
@@ -3,6 +3,16 @@
 @echo off
 pushd "%~dp0"
 
+#if !@(SDK.Directory:IsSet) || !@(NDK.Directory:IsSet)
+echo ERROR: Could not locate the Android SDK or NDK. >&2
+echo. >&2
+echo These dependencies can be acquired by installing 'android-build-tools': >&2
+echo. >&2
+echo     npm install android-build-tools -g >&2
+echo. >&2
+goto ERROR
+#endif
+
 #if @(JDK.Directory:IsSet)
 set JAVA_HOME=@(JDK.Directory:NativePath)
 #endif
--- lib/UnoCore/Targets/Android/build.sh
+++ lib/UnoCore/Targets/Android/build.sh
@@ -4,6 +4,16 @@
 set -e
 cd "`dirname "$0"`"
 
+#if !@(SDK.Directory:IsSet) || !@(NDK.Directory:IsSet)
+echo "ERROR: Could not locate the Android SDK or NDK." >&2
+echo "" >&2
+echo "These dependencies can be acquired by installing 'android-build-tools':" >&2
+echo "" >&2
+echo "    npm install android-build-tools -g" >&2
+echo "" >&2
+exit 1
+#endif
+
 #if @(JDK.Directory:IsSet)
 export JAVA_HOME="@(JDK.Directory)"
 #endif
