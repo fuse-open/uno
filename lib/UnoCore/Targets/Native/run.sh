170170c575097f2e0112779fa8cd06883391c479
Morten Daniel Fornes <mdfornes@gmail.com>
2019-06-09 12:22:53 +0700

    Native: always use Xcode on macOS
    Now we don't need to delete the CMake cache due to switching generators,
    which speeds up subsequent builds.
    
    Also, don't try to launch Xcode if passing --debug on Linux.

--- lib/UnoCore/Targets/Native/run.sh
+++ lib/UnoCore/Targets/Native/run.sh
@@ -7,11 +7,15 @@ cd "`dirname "$0"`"
 case $1 in
 debug)
     shift
-    rm -f CMakeCache.txt
+#if @(MAC:Defined)
     @(CMake) -GXcode "$@" .
     echo "Opening Xcode"
     open -aXcode "@(Project.Name).xcodeproj"
     exit $?
+#else
+    echo "Debugging is not supported on this platform." >&2
+    exit 1
+#endif
     ;;
 valgrind)
     shift
