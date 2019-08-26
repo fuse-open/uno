43e353042194b53a25f65139d70d1bb10c74f725
Morten Daniel Fornes <mdfornes@gmail.com>
2019-08-26 17:42:02 +0200

    iOS: switch to user-installed ios-deploy

--- lib/UnoCore/Targets/iOS/run.sh
+++ lib/UnoCore/Targets/iOS/run.sh
@@ -17,17 +17,14 @@ debug)
     ;;
 uninstall)
     echo "Uninstalling @(BundleIdentifier)"
-    chmod +x "@(Base.Directory)/bin/ios-deploy"
-    "@(Base.Directory)/bin/ios-deploy" -9 -1 "@(BundleIdentifier)"
+    ios-deploy -9 -1 "@(BundleIdentifier)"
     exit $?
     ;;
 esac
 
-chmod +x "@(Base.Directory)/bin/ios-deploy"
-
 #if @(Cocoapods:Defined)
 pod install
-"@(Base.Directory)/bin/ios-deploy" --noninteractive --debug --bundle "build/Build/Products/@(Pbxproj.Configuration)-iphoneos/@(Project.Name).app" "$@"
+ios-deploy --noninteractive --debug --bundle "build/Build/Products/@(Pbxproj.Configuration)-iphoneos/@(Project.Name).app" "$@"
 #else
-"@(Base.Directory)/bin/ios-deploy" --noninteractive --debug --bundle "build/@(Pbxproj.Configuration)-iphoneos/@(Project.Name).app" "$@"
+ios-deploy --noninteractive --debug --bundle "build/@(Pbxproj.Configuration)-iphoneos/@(Project.Name).app" "$@"
 #endif
