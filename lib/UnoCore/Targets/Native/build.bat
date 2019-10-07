060e65d59cedeb609071a2bf490e8fd1ae44b9c7
Morten Daniel Fornes <mdfornes@gmail.com>
2019-10-08 05:57:09 +0700

    Native: detect CMake on Windows
    Provide instructions on how to install CMake when not found.
    
    Related to #227

--- lib/UnoCore/Targets/Native/build.bat
+++ lib/UnoCore/Targets/Native/build.bat
@@ -1,6 +1,18 @@
 :: @(MSG_ORIGIN)
 :: @(MSG_EDIT_WARNING)
 @echo off
+
+where cmake 1>NUL 2>NUL
+if not %ERRORLEVEL% == 0 (
+    echo ERROR: Unable to find the 'cmake' command. Make sure CMake is installed and added to PATH. >&2
+    echo. >&2
+    echo On Windows, you can install CMake using Chocolatey: >&2
+    echo. >&2
+    echo     choco install cmake >&2
+    echo. >&2
+    exit /b 1
+)
+
 pushd "%~dp0"
 
 cmake -G"@(CMake.Generator)" .
--- lib/UnoCore/Targets/Native/run.bat
+++ lib/UnoCore/Targets/Native/run.bat
@@ -3,6 +3,18 @@
 @echo off
 
 if "%1" == "debug" (
+    where cmake 1>NUL 2>NUL
+    if not %ERRORLEVEL% == 0 (
+        popd
+        echo ERROR: Unable to find the 'cmake' command. Make sure CMake is installed and added to PATH. >&2
+        echo. >&2
+        echo On Windows, you can install CMake using Chocolatey: >&2
+        echo. >&2
+        echo     choco install cmake >&2
+        echo. >&2
+        exit /b 1
+    )
+
     pushd "%~dp0"
 
     cmake -G"@(CMake.Generator)" .
