d2cf4b91b4905bc1f4f043e94de902ea98c0fe16
Morten Daniel Fornes <mdfornes@gmail.com>
2019-12-21 16:40:30 +0700

    Uno.Compiler: drop BundleFile.ContentType

--- src/compiler/Uno.Compiler.API/Domain/Extensions/BundleFile.cs
+++ src/compiler/Uno.Compiler.API/Domain/Extensions/BundleFile.cs
@@ -5,19 +5,17 @@
         public readonly string BundleName;
         public readonly string TargetName;
         public readonly string SourcePath;
-        public readonly ContentType ContentType;
 
-        public BundleFile(string bundleName, string sourcePath, ContentType type)
-            : this(bundleName, bundleName, sourcePath, type)
+        public BundleFile(string bundleName, string sourcePath)
+            : this(bundleName, bundleName, sourcePath)
         {
         }
 
-        public BundleFile(string bundleName, string targetName, string sourcePath, ContentType type)
+        public BundleFile(string bundleName, string targetName, string sourcePath)
         {
             BundleName = bundleName;
             TargetName = targetName;
             SourcePath = sourcePath;
-            ContentType = type;
         }
 
         public override string ToString()
