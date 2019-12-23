8088f483d8d7f8d7e341419b349cfd70cbb67cbc
Morten Daniel Fornes <mdfornes@gmail.com>
2019-12-23 10:49:43 +0700

    1.14.0-canary.2

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Runtime.InteropServices;
 // Version information.
 [assembly: AssemblyVersion("1.14.0.0")]
 [assembly: AssemblyFileVersion("1.14.0.0")]
-[assembly: AssemblyInformationalVersion("1.14.0-canary.1")]
+[assembly: AssemblyInformationalVersion("1.14.0-canary.2")]
