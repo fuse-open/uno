7cb2b84ca7a2f68881960412fc7b55b95dacfb4c
Morten Daniel Fornes <mdfornes@gmail.com>
2020-01-05 02:09:35 +0700

    pack.sh: call build.sh instead of duplicating code

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,30 +3,12 @@ SELF=`echo $0 | sed 's/\\\\/\\//g'`
 cd "`dirname "$SELF"`/.." || exit 1
 source scripts/common.sh
 
-# Configuration
-CONFIGURATION="Debug"
-
-# Parse options
-for arg in "$@"; do
-    case $arg in
-    -h|--help)
-        echo "Build options:"
-        echo "  --debug     Use 'Debug' configuration (default)"
-        echo "  --release   Use 'Release' configuration"
-        exit 0
-        ;;
-    --debug)
-        CONFIGURATION="Debug"
-        ;;
-    --release)
-        CONFIGURATION="Release"
-        ;;
-    *)
-        echo "ERROR: Invalid argument '$arg'" >&2
-        exit 1
-        ;;
-    esac
-done
+if [ "$1" = --release ]; then
+    CONFIGURATION="Release"
+    shift
+elif [ -z "$CONFIGURATION" ]; then
+    CONFIGURATION="Debug"
+fi
 
 h1 "Installing packages"
 nuget restore uno.sln
@@ -35,4 +17,4 @@ h1 "Building platform tools"
 csharp-build uno.sln
 
 h1 "Building core library"
-uno doctor -ec$CONFIGURATION lib
+uno doctor -ec$CONFIGURATION lib "$@"
