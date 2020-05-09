bb4ee239d84513e3c344677416e24c8aaade4348
Morten Daniel Fornes <mdfornes@gmail.com>
2020-05-10 05:47:20 +0700

    build assemblies in bin/

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -4,10 +4,9 @@ cd "`dirname "$SELF"`/.." || exit 1
 source scripts/common.sh
 
 # Initialize
-DST="release"
 shopt -s dotglob
-rm -rf ${DST:?}/* 2> /dev/null || :
-mkdir -p $DST
+rm -rf bin/* 2> /dev/null || :
+mkdir -p bin
 
 # Detect version info
 VERSION=`bash scripts/get-version.sh`
@@ -43,32 +42,3 @@ bash scripts/build.sh --release --version=$VERSION
 
 # Remove GlobalAssemblyInfo.Override.cs
 rm -f src/GlobalAssemblyInfo.Override.cs
-
-h1 "Preparing release"
-######################
-
-# Copy assemblies
-cp src/main/Uno.CLI.Main/bin/Release/*.{dll,exe,dylib} $DST
-cp -f src/testing/Uno.TestRunner.CLI/bin/Release/*.{dll,exe} $DST
-
-# Put app loaders for macOS and Windows in subdirectories to avoid conflicts
-mkdir -p $DST/apploader-mac
-cp -f src/runtime/Uno.AppLoader-MonoMac/bin/Release/*.{dll,exe,dylib} $DST/apploader-mac
-cp -f src/runtime/Uno.AppLoader-MonoMac/bin/Release/monostub $DST/apploader-mac
-
-mkdir -p $DST/apploader-win
-cp -f src/runtime/Uno.AppLoader-WinForms/bin/Release/*.{dll,exe} $DST/apploader-win
-
-# Generate config file
-cat <<EOF >> $DST/.unoconfig
-Assemblies.Test: uno-test.exe
-Assemblies.Uno: uno.exe
-
-if WIN32 {
-    Paths.AppLoader: apploader-win
-} else if MAC {
-    Paths.AppLoader: apploader-mac
-}
-
-Packages.SearchPaths += ../lib/build
-EOF
