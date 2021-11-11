d6311b25b79b841d8128ea6c692f37dcf2b92712
Morten Daniel Fornes <mdfornes@gmail.com>
2021-11-11 20:56:58 +0700

    unoconfig: add iOS.Simulator.Device

--- lib/UnoCore/Targets/iOS/run.sh
+++ lib/UnoCore/Targets/iOS/run.sh
@@ -28,9 +28,9 @@ fi
 
 #if @(COCOAPODS:Defined)
 pod install
-ios-sim launch -diPhone-12 "build/Build/Products/@(Pbxproj.Configuration)-iphonesimulator/@(Project.Name).app" "$@"
+ios-sim launch -d"@(iOS.Simulator.Device || 'iPhone-12')" "build/Build/Products/@(Pbxproj.Configuration)-iphonesimulator/@(Project.Name).app" "$@"
 #else
-ios-sim launch -diPhone-12 "build/@(Pbxproj.Configuration)-iphonesimulator/@(Project.Name).app" "$@"
+ios-sim launch -d"@(iOS.Simulator.Device || 'iPhone-12')" "build/@(Pbxproj.Configuration)-iphonesimulator/@(Project.Name).app" "$@"
 #endif
 
 #else // @(IOS_SIMULATOR:Defined)
