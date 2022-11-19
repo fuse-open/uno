529e00dcdf1cc421f06caa60a3541c38d4fcac95
Morten Daniel Fornes <mdfornes@gmail.com>
2022-11-20 01:33:20 +0700

    scripts: drop semicolons in restore.js

--- scripts/restore.js
+++ scripts/restore.js
@@ -1,50 +1,50 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
 function findup(suffix) {
     for (let dir = __dirname, parent = undefined;;
              dir = path.dirname(dir)) {
 
         if (dir == parent)
-            throw Error(`${suffix} was not found`);
+            throw Error(`${suffix} was not found`)
 
-        parent = dir;
-        const file = path.join(dir, suffix);
+        parent = dir
+        const file = path.join(dir, suffix)
 
         if (fs.existsSync(file))
-            return file;
+            return file
     }
 }
 
 function findNodeModule(name) {
-    const package = findup(`node_modules/${name}/package.json`);
-    return path.dirname(package);
+    const package = findup(`node_modules/${name}/package.json`)
+    return path.dirname(package)
 }
 
 function restoreFiles(src, dst) {
     if (!fs.existsSync(dst))
-        return;
+        return
 
     for (file of fs.readdirSync(src)) {
-        const srcf = path.join(src, file);
-        const dstf = path.join(dst, file);
-        const placeholder = dstf + ".restore";
+        const srcf = path.join(src, file)
+        const dstf = path.join(dst, file)
+        const placeholder = dstf + ".restore"
 
         if (!fs.existsSync(placeholder))
-            continue;
+            continue
 
-        const relative = path.relative(process.cwd(), dstf);
-        console.log(`restoring ${relative}`);
-        fs.copyFileSync(srcf, dstf);
-        fs.unlinkSync(placeholder);
+        const relative = path.relative(process.cwd(), dstf)
+        console.log(`restoring ${relative}`)
+        fs.copyFileSync(srcf, dstf)
+        fs.unlinkSync(placeholder)
     }
 }
 
 // Restore Xamarin.Mac binaries.
-const xamarin = findNodeModule("@fuse-open/xamarin-mac");
-restoreFiles(xamarin, path.join(__dirname, "..", "bin", "net6.0"));
-restoreFiles(xamarin, path.join(__dirname, "..", "bin", "mac", "net6.0"));
+const xamarin = findNodeModule("@fuse-open/xamarin-mac")
+restoreFiles(xamarin, path.join(__dirname, "..", "bin", "net6.0"))
+restoreFiles(xamarin, path.join(__dirname, "..", "bin", "mac", "net6.0"))
 
 // Restore OpenTK and ANGLE (Windows only).
-const opentk = findNodeModule("@fuse-open/opentk");
-restoreFiles(opentk, path.join(__dirname, "..", "bin", "win", "net6.0-windows"));
+const opentk = findNodeModule("@fuse-open/opentk")
+restoreFiles(opentk, path.join(__dirname, "..", "bin", "win", "net6.0-windows"))
