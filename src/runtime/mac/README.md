10933b06d39942e5c5ea8f15b9dc22dc563c0826
Morten Daniel Fornes <mdfornes@gmail.com>
2023-03-27 09:52:20 +0700

    macOS: prebuilt app loader
    This adds a prebuilt app loader for macOS, downloaded via NuGet.
    
    The binaries that come out on macOS are quite large and don't change
    often, so move them to NuGet to avoid bloating the main Uno package.
    
    Native binaries are also CPU specific (x64), so we'll publish another
    set for arm64 at a later point.
    
    * Configure assemblies via Info.plist and load via .NET reflection
      instead of editing assemblies, since edited macOS assemblies results
      in crashes on .NET 6.0.
    
    * Move relevant source code to a new repository at
      https://github.com/fuse-open/mac-loader.

---
+++ src/runtime/mac/README.md
@@ -0,0 +1,3 @@
+# Moved
+
+The source code that used to live in this directory was moved to [fuse-open/mac-loader](https://github.com/fuse-open/mac-loader).
--- src/runtime/mac/WheelDeltaMode.cs
+++
@@ -1,9 +0,0 @@
-namespace Uno.AppLoader.MonoMac
-{
-    public enum WheelDeltaMode
-    {
-        DeltaPixel = 0,
-        DeltaLine = 1,
-        DeltaPage = 2
-    }
-}
