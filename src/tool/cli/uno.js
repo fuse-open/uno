59dc4801f06d307797a96b23a0a9ea51fb659066
Morten Daniel Fornes <mdfornes@gmail.com>
2023-04-02 20:42:14 +0700

    upgrade dotnet-run -> 2.0.0

--- src/tool/cli/uno.js
+++ src/tool/cli/uno.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const path = require('path');
-const run = require('dotnet-run');
+const path = require("path")
+const run = require("dotnet-run")
 
-run(path.join(__dirname, 'uno.dll'),
-    process.argv.slice(2),
-    process.exit);
+run(path.join(__dirname, "uno.dll"),
+    process.argv.slice(2))
+    .then(process.exit)
