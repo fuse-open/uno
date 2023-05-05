edbc1945bb23a9997a0afd6d55a30cfac5e992e8
Morten Daniel Fornes <mdfornes@gmail.com>
2023-05-05 23:16:59 +0700

    C++-backend: fix Android compile errors on macOS
    This makes DeclarationComparer more robust and makes sure to use it when
    combining declarations in CppGenerator, missed in fa03654.
    
    This fixes compile errors when compiling Android apps on macOS.

--- src/compiler/backend/cpp/DeclarationComparer.cs
+++ src/compiler/backend/cpp/DeclarationComparer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Uno.Compiler.Backends.CPlusPlus
@@ -5,18 +6,27 @@ namespace Uno.Compiler.Backends.CPlusPlus
     /** A comparer that makes strings starting with '#' come out first. */
     public class DeclarationComparer : IComparer<string>
     {
-        public static readonly DeclarationComparer Singleton = new DeclarationComparer();
+        public static readonly DeclarationComparer Singleton = new();
 
         public int Compare(string x, string y)
         {
-            var xIsDirective = x.Length > 0 && x[0] == '#';
-            var yIsDirective = y.Length > 0 && y[0] == '#';
+            var xIsDirective = FirstNonWhiteSpaceChar(x) == '#';
+            var yIsDirective = FirstNonWhiteSpaceChar(y) == '#';
 
             return xIsDirective && !yIsDirective
                 ? -1
                 : !xIsDirective && yIsDirective
                 ? 1
-                : x.CompareTo(y);
+                : StringComparer.InvariantCulture.Compare(x, y);
+        }
+
+        static char FirstNonWhiteSpaceChar(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+                if (!char.IsWhiteSpace(value[i]))
+                    return value[i];
+
+            return '\0';
         }
     }
 }
