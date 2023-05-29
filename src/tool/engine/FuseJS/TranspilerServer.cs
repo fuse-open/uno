fuse-open/uno

commit 000294e8fabb3dd6ffd1d5ee90dd19ab06ea67ad

engine: refactor FuseJS transpiler code
This makes it easier to reuse the transpiler functionality in other
parts of the code base, e.g. the UX compiler.

* Add ITranspiler interface in Uno.Compiler.API
* Extract LazyTranspiler and TranspilerServer classes
* Pass LazyTranspiler down from BuildDriver and LibraryBuilder
* Simplify BundleCache class

src/tool/engine/FuseJS/TranspilerServer.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Uno.Compiler.API;
 using Uno.Configuration;
 using Uno.Diagnostics;
 using Uno.Logging;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Uno.Build.FuseJS
 {
-    public class Transpiler : LogObject, IDisposable
+    public class TranspilerServer : LogObject, ITranspiler
     {
         static readonly HttpClient _httpClient = new()
         {
@@ -27,7 +28,7 @@ namespace Uno.Build.FuseJS
         bool _disposed;
         bool _isFaulted;
 
-        public Transpiler(Log log, UnoConfig config)
+        public TranspilerServer(Log log, UnoConfig config)
             : base(log)
         {
             var script = Path.Combine(

src/compiler/api/ITranspiler.cs
@@ -1,0 +1,9 @@
+﻿using System;
+
+namespace Uno.Compiler.API
+{
+    public interface ITranspiler : IDisposable
+    {
+        bool TryTranspile(string filename, string code, out string output);
+    }
+}

src/compiler/api/IUtilities.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Uno.Collections;
 using Uno.Compiler.API.Domain.IL;
 using Uno.Compiler.API.Domain.IL.Statements;
 

src/tool/engine/BuildDriver.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Uno.Build.FuseJS;
 using Uno.Build.Libraries;
 using Uno.Build.Stuff;
 using Uno.Compiler;
@@ -34,6 +35,7 @@ namespace Uno.Build
         readonly UnoConfig _config;
         readonly BuildFile _file;
         readonly BundleCache _cache;
+        readonly LazyTranspiler _transpiler;
         IDisposable _anim;
 
         public bool IsUpToDate => !_options.Force && _file.Exists &&
@@ -93,7 +95,8 @@ namespace Uno.Build
                 };
             }
 
-            _cache = _options.BundleCache ?? new BundleCache(Log, _config);
+            _transpiler = new LazyTranspiler(Log, _config);
+            _cache = _options.BundleCache ?? new BundleCache(Log, _config, _transpiler);
             PrintRow("Search paths", _cache.SearchPaths);
 
             _compiler = new Compiler(
@@ -119,9 +122,7 @@ namespace Uno.Build
 
         public void Dispose()
         {
-            // Dispose PackageCache if it was created by us
-            if (_options.BundleCache == null)
-                _cache.Dispose();
+            _transpiler.Dispose();
         }
 
         public void Clean()

src/tool/engine/FuseJS/LazyTranspiler.cs
@@ -1,0 +1,38 @@
+﻿using Uno.Compiler.API;
+using Uno.Configuration;
+using Uno.Logging;
+
+namespace Uno.Build.FuseJS
+{
+    public class LazyTranspiler : ITranspiler
+    {
+        readonly Log _log;
+        readonly UnoConfig _config;
+        TranspilerServer _server;
+
+        public LazyTranspiler(Log log, UnoConfig config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public bool TryTranspile(string filename, string code, out string output)
+        {
+            // Ensure that the server is started
+            if (_server == null)
+                _server = new TranspilerServer(_log, _config);
+
+            return _server.TryTranspile(filename, code, out output);
+        }
+
+        public void Dispose()
+        {
+            // Stop the server again if necessary
+            if (_server != null)
+            {
+                _server.Dispose();
+                _server = null;
+            }
+        }
+    }
+}

src/tool/engine/Libraries/BundleCache.cs
@@ -12,14 +12,13 @@ using Uno.ProjectFormat;
 
 namespace Uno.Build.Libraries
 {
-    public class BundleCache : DiskObject, IDisposable
+    public class BundleCache : DiskObject
     {
         // For use by UXNinja & tests in Fuse Studio
         public static SourceBundle GetBundle(Log log, Project project)
         {
-            // Passing false to disable FuseJS transpiler.
-            using (var cache = new BundleCache(log, project.Config, false))
-                return cache.GetBundle(project);
+            // Passing null to disable FuseJS transpiler.
+            return new BundleCache(log, null, null).GetBundle(project);
         }
 
         readonly SearchPaths _sourcePaths = new SearchPaths();
@@ -27,25 +26,22 @@ namespace Uno.Build.Libraries
         readonly Dictionary<string, string> _locks = new Dictionary<string, string>();
         readonly Dictionary<string, SourceBundle> _cache = new Dictionary<string, SourceBundle>();
         readonly ListDictionary<string, DirectoryInfo> _library = new ListDictionary<string, DirectoryInfo>();
-        readonly UnoConfig _config;
-        readonly bool _enableTranspiler;
-        Transpiler _transpiler;
+        readonly LazyTranspiler _transpiler;
 
         public IEnumerable<string> SearchPaths => _sourcePaths.Concat(_searchPaths).Where(Directory.Exists);
 
         public BundleCache()
-            : this(null, null, false)
+            : this(null, null, null)
         {
         }
 
-        public BundleCache(Log log, UnoConfig config, bool enableTranspiler = true)
+        public BundleCache(Log log, UnoConfig config, LazyTranspiler transpiler)
             : base(log ?? Log.Null)
         {
+            _transpiler = transpiler;
+
             if (config == null)
                 config = UnoConfig.Current;
-
-            _config = config;
-            _enableTranspiler = enableTranspiler;
 
             foreach (var src in config.GetFullPathArray("SearchPaths.Sources", "Packages.SourcePaths"))
                 _sourcePaths.AddOnce(Path.Combine(
@@ -56,12 +52,6 @@ namespace Uno.Build.Libraries
 
             foreach (var src in config.GetFullPathArray("SearchPaths", "Packages.SearchPaths"))
                 _searchPaths.AddOnce(src);
-        }
-
-        public void Dispose()
-        {
-            _transpiler?.Dispose();
-            _transpiler = null;
         }
 
         public SourceBundle GetBundle(string name, string version = null)
@@ -159,7 +149,7 @@ namespace Uno.Build.Libraries
             foreach (var f in project.FuseJSFiles)
             {
                 // We don't need to spend time on this in UXNinja, tests, etc.
-                if (!_enableTranspiler)
+                if (_transpiler == null)
                     continue;
 
                 var name = f.NativePath;
@@ -179,10 +169,6 @@ namespace Uno.Build.Libraries
                 if (File.Exists(outputFile) &&
                     File.GetLastWriteTime(outputFile) >= File.GetLastWriteTime(inputFile))
                     continue;
-
-                // Ensure Transpiler is initialized before starting a task
-                if (_transpiler == null)
-                    _transpiler = new Transpiler(Log, _config);
 
                 name = inputFile.ToRelativePath();
                 Log.Verbose("Transpiling " + name);

src/tool/engine/Libraries/LibraryBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Uno.Build.FuseJS;
 using Uno.Collections;
 using Uno.Configuration;
 using Uno.IO;
@@ -81,8 +82,9 @@ namespace Uno.Build.Libraries
 
             var allProjects = LoadProjects(sourceDirectories);
             var buildProjects = GetBuildList(allProjects);
+            var transpiler = new LazyTranspiler(Log, config);
             var bundleCache = CanCache
-                                    ? new BundleCache(Log, config)
+                                    ? new BundleCache(Log, config, transpiler)
                                     : null;
 
             try
@@ -105,7 +107,7 @@ namespace Uno.Build.Libraries
             finally
             {
                 Log.Message($"Completed in {Log.Time - startTime:0.00} seconds");
-                bundleCache?.Dispose();
+                transpiler.Dispose();
             }
         }
 

commit 8cd91e4fd57ece2b0582fb066724e07eef8c9690

engine: fix NRE in TranspilerServer
Regressed in 000294e (#482)

src/tool/engine/FuseJS/TranspilerServer.cs
@@ -28,9 +28,11 @@ namespace Uno.Build.FuseJS
         bool _disposed;
         bool _isFaulted;
 
-        public TranspilerServer(Log log, UnoConfig config)
+        public TranspilerServer(Log log, UnoConfig config = null)
             : base(log)
         {
+            config ??= UnoConfig.Current;
+
             var script = Path.Combine(
                 config.GetNodeModuleDirectory("@fuse-open/transpiler"),
                 "server.min.js");