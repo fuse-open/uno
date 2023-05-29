fuse-open/uno

commit daa1e7558eff38fa2688524d75f0ba92a55214fe

compiler: support console programs
This makes it possible to build normal console programs in Uno, using
"static int Main(string[] args)" entrypoints (return type and args are
optional), just like in C#.

* Set "outputType" to "console" (unoproj) to create a console program
* Supported by dotnet and native build targets

src/tool/engine/BuildDriver.cs
@@ -259,9 +259,14 @@ namespace Uno.Build
         public void BuildNative()
         {
             using (Log.StartAnimation("Building " + _target.ToString().ToLower() + (
-                    _backend.BuildType == BuildType.Executable && !_env.IsDefined("LIBRARY")
-                        ? " app"
-                        : " library") +
+                        _env.IsTest
+                            ? "test" :
+                        _env.IsConsole
+                            ? "console app" :
+                        _env.IsLibrary
+                            ? "library"
+                            : "app"
+                    ) +
                     " (" + _compilerOptions.Configuration.ToString().ToLower() + ")"))
             {
                 if (!_target.Build(_compiler.Shell, _file, _options.NativeArguments))

lib/UnoCore/UnoCore.unoproj
@@ -91,7 +91,11 @@
     "cil/mac-loader.stuff:stuff",
     "cil/run.bat:file",
     "cil/run.sh:file",
+    "cil/test.cil.uxl:extensions",
     "cpp/config.cpp.uxl:extensions",
+    "cpp/main-app.cpp:file",
+    "cpp/main-console.cpp:file",
+    "cpp/main-mobile.cpp:file",
     "cpp/uno.cpp.uxl:extensions",
     "cpp/uno.h:file",
     "cpp/Uno/_config.h:file",
@@ -110,8 +114,6 @@
     "cpp/Uno/Support.h:file",
     "cpp/Uno/SupportApple.mm:file",
     "cpp/Uno/Uno.h:file",
-    "cpp/uPlatform/_main.cpp:file",
-    "cpp/uPlatform/_mainMobile.cpp:file",
     "cpp/uPlatform/GLHelper.h:file",
     "cpp/uPlatform/GraphicsContext.h:file",
     "cpp/uPlatform/WinAPIHelper.h:file",

lib/UnoCore/cil/app.cil.uxl
@@ -1,4 +1,4 @@
-<Extensions Backend="CIL" Condition="!LIBRARY && !TEST">
+<Extensions Backend="CIL" Condition="!CONSOLE && !LIBRARY && !TEST">
 
     <Set AppDirectory="@(Project.Name).app" Condition="HOST_MAC" />
     <Set AssemblyDirectory="@(HOST_MAC:Defined:Test('@(AppDirectory)/Contents/MonoBundle', '.'))" />

lib/UnoCore/cil/mac-loader.stuff
@@ -1,3 +1,3 @@
-if DOTNET && !LIBRARY && HOST_MAC && HOST_X64 {
+if DOTNET && HOST_MAC && HOST_X64 && !CONSOLE && !LIBRARY && !TEST {
     "mac-x64": "https://www.nuget.org/api/v2/package/uno-mac-loader-x64/3.0.0-beta.0"
 }

lib/UnoCore/cil/test.cil.uxl
@@ -1,9 +1,13 @@
-<Extensions Backend="CIL" Condition="CONSOLE">
+<Extensions Backend="CIL" Condition="TEST">
+
+    <Set AppLoader.Assembly="@(Config.AppLoader.Console:Path)/uno-app.dll" />
+    <Set AppLoader.Class="Uno.AppLoader.Program" />
+    <Set AppLoader.Method="UnoGenerated" />
 
     <CopyFile Name="@(Config.AppLoader.Console:Path)/uno-app.runtimeconfig.json"
-        TargetName="@(Project.Name).runtimeconfig.json" />
+        TargetName="@(Project.Name).exe.runtimeconfig.json" />
 
-    <Set Product="@(Project.Name).dll" />
+    <Set Product="@(Project.Name).exe.dll" />
 
     <!-- Windows -->
     <Set Commands.Run="run.bat" Condition="HOST_WIN32" />

lib/UnoCore/cpp/main-console.cpp
@@ -1,0 +1,28 @@
+// @(MSG_ORIGIN)
+// @(MSG_EDIT_WARNING)
+
+#include <Uno/ObjectModel.h>
+@(Main.IncludeDirective)
+@(TypeObjects.Declaration:JoinSorted())
+void uInitRtti(uType*(*factories[])());
+
+// See @{Uno.Environment.GetCommandLineArgs():Call()}
+int uArgc = 0;
+char** uArgv = nullptr;
+
+int main(int argc, char* argv[])
+{
+    uArgc = argc;
+    uArgv = argv;
+
+    uRuntime uno;
+    uAutoReleasePool pool;
+
+    uType* (*factories[])() = {
+        @(TypeObjects.FunctionPointer:JoinSorted('\n        ', '', ','))
+        nullptr
+    };
+
+    uInitRtti(factories);
+    return @(Main.Entrypoint);
+}

commit 6335395a239f0304d6af7f6e1b0b7fc482f6aff8

engine: add missing space in BuildDriver
Regressed in daa1e75 (#489)

src/tool/engine/BuildDriver.cs
@@ -258,7 +258,7 @@ namespace Uno.Build
 
         public void BuildNative()
         {
-            using (Log.StartAnimation("Building " + _target.ToString().ToLower() + (
+            using (Log.StartAnimation("Building " + _target.ToString().ToLower() + " " + (
                         _env.IsTest
                             ? "test" :
                         _env.IsConsole