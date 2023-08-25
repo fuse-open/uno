fuse-open/uno

commit 40403e78445d6ec1e56d5ff769d0807b584dd5ce

compiler: refactor build queue
Extract BuildQueue from BlockBuilder and TypeBuilder to get better
control over when different classes, functions, etc, are compiled,
improving abilities when it comes to lazy compilation.

src/compiler/core/Syntax/Builders/TypeBuilder.Parameterize.cs
@@ -186,9 +186,10 @@ namespace Uno.Compiler.Core.Syntax.Builders
 
             result.SetMasterDefinition(definition.MasterDefinition);
             ParameterizeInnerTypes(definition, definition, map, result);
-            EnqueueType(result,
+            _queue.EnqueueType(result,
                 x => ParameterizeBaseType(definition, map, x),
                 x => ParameterizeMembers(definition, definition, map, x));
+
             return result;
         }
 
@@ -292,7 +293,7 @@ namespace Uno.Compiler.Core.Syntax.Builders
 
             if (result != null)
             {
-                map.Add(arg, result);
+                map[arg] = result;
                 return result;
             }
 
@@ -361,17 +362,23 @@ namespace Uno.Compiler.Core.Syntax.Builders
             {
                 var e = current.NestedTypes[i];
                 var t = CreateParameterizableInnerType(e, result);
+                result.NestedTypes.Add(t);
 
                 if (e.IsGenericDefinition)
                     t.MakeGenericDefinition(e.GenericParameters);
 
                 t.SetMasterDefinition(e.MasterDefinition);
 
-                EnqueueType(t,
+                _queue.EnqueueType(t,
                     x => ParameterizeBaseType(e, map, x),
                     x => ParameterizeMembers(definition, e, map, x));
+            }
+
+            for (int i = 0; i < current.NestedTypes.Count; i++)
+            {
+                var e = current.NestedTypes[i];
+                var t = result.NestedTypes[i];
                 ParameterizeInnerTypes(definition, e, map, t);
-                result.NestedTypes.Add(t);
             }
         }
 

src/compiler/core/Compiler.cs
@@ -49,6 +49,7 @@ namespace Uno.Compiler.Core
         public readonly NameResolver NameResolver;
 
         // Building
+        public readonly BuildQueue Queue;
         public readonly BlockBuilder BlockBuilder;
         public readonly BundleBuilder BundleBuilder;
         public readonly TypeBuilder TypeBuilder;
@@ -91,8 +92,9 @@ namespace Uno.Compiler.Core
             var data = Data = new BuildData(il, extensions, ilf);
             var environment = Environment = new BuildEnvironment(backend, bundle, options, extensions, ilf, this);
             var input = Input = new SourceReader(log, bundle, environment);
-            var blockBuilder = BlockBuilder = new BlockBuilder(backend, il, ilf, resolver, this);
-            var typeBuilder = TypeBuilder = new TypeBuilder(environment, ilf, resolver, this);
+            var queue = Queue = new BuildQueue(log, environment, backend, this);
+            var blockBuilder = BlockBuilder = new BlockBuilder(backend, il, ilf, resolver, this, queue);
+            var typeBuilder = TypeBuilder = new TypeBuilder(environment, ilf, resolver, this, queue);
             BundleBuilder = new BundleBuilder(backend, environment, ilf, this);
             AstProcessor = new AstProcessor(il, blockBuilder, typeBuilder, resolver, environment);
             UxlProcessor = new UxlProcessor(disk, backend.Name, il, extensions, environment, ilf);
@@ -132,20 +134,23 @@ namespace Uno.Compiler.Core
                 if (Backend.CanLink(bundle))
                     bundle.Flags |= SourceBundleFlags.CanLink;
 
-            using (Log.StartProfiler(TypeBuilder))
-                TypeBuilder.Build();
-            if (Log.HasErrors)
-                return;
+            using (Log.StartProfiler(Queue))
+                Queue.BuildTypesAndFunctions();
 
-            Backend.ShaderBackend.Initialize(this, BundleBuilder);
-
-            using (Log.StartProfiler(BlockBuilder))
-                BlockBuilder.Build();
             if (Log.HasErrors)
                 return;
 
             using (Log.StartProfiler(UxlProcessor))
                 UxlProcessor.CompileDocuments();
+
+            if (Log.HasErrors)
+                return;
+
+            Backend.ShaderBackend.Initialize(this, BundleBuilder);
+
+            using (Log.StartProfiler(Queue))
+                Queue.BuildEverything();
+
             if (Log.HasErrors)
                 return;
 
@@ -205,7 +210,6 @@ namespace Uno.Compiler.Core
             UxlProcessor.CompileRequirements();
             ILStripper.Begin();
             Run(_transforms);
-            TypeBuilder.BuildTypes();
 
             using (Log.StartProfiler(ILStripper))
                 ILStripper.End();

src/compiler/core/IL/EntitySwapper.cs
@@ -65,7 +65,6 @@ namespace Uno.Compiler.Core.IL
                                 args[i] = GetType(dt.FlattenedArguments[i]);
 
                             result = TypeBuilder.Parameterize(result.Source, def, args);
-                            TypeBuilder.BuildTypes();
                             SwapTypes.Add(dt, result);
                             return result;
                         }
@@ -99,7 +98,6 @@ namespace Uno.Compiler.Core.IL
 
                 var def = result.IsGenericDefinition ? result : GetType(dt.GenericDefinition);
                 result = TypeBuilder.Parameterize(dt.Source, def, args);
-                TypeBuilder.BuildTypes();
             }
 
             SwapTypes.Add(dt, result);

src/compiler/core/Syntax/Builders/BlockBuilder.CompileDraw.cs
@@ -32,10 +32,8 @@ namespace Uno.Compiler.Core.Syntax.Builders
 
             var result = new DrawBlock(draw.Source, parent, method, FlattenVariableScopes(vscopeStack));
             method.DrawBlocks.Add(result);
-
-            EnqueueBlock(result, x => PopulateBlock(draw.Block, x));
-            _enqueuedDrawClasses.Add(method.DeclaringType);
-
+            _queue.EnqueueBlock(result, x => PopulateBlock(draw.Block, x));
+            _queue.EnqueueDrawClass(method.DeclaringType);
             return result.DrawScope;
         }
 

src/compiler/core/Syntax/Builders/BlockBuilder.CreateBlock.cs
@@ -30,8 +30,7 @@ namespace Uno.Compiler.Core.Syntax.Builders
                 Log.Error(src, ErrorCode.E3214, "Block declaration is not allowed in this scope");
 
             _processedBlocks.Add(ast, result);
-            EnqueueBlock(result, x => PopulateBlock(ast, x));
-
+            _queue.EnqueueBlock(result, x => PopulateBlock(ast, x));
             return result;
         }
 
@@ -76,7 +75,7 @@ namespace Uno.Compiler.Core.Syntax.Builders
                     return MetaBlock.Invalid;
             }
 
-            EnqueueBlock(result, x => PopulateBlock(ast, x));
+            _queue.EnqueueBlock(result, x => PopulateBlock(ast, x));
             return result;
         }
     }

src/compiler/core/Syntax/Builders/BlockBuilder.CreateMetaProperty.cs
@@ -58,12 +58,11 @@ namespace Uno.Compiler.Core.Syntax.Builders
             }
 
             var result = new MetaProperty(ast.Name.Source, parent, dt, ast.Name.Symbol, visibility);
-            _enqueuedProperties.Add(new KeyValuePair<AstMetaProperty, MetaProperty>(ast, result));
-
+            _queue.EnqueueMetaProperty(ast, result);
             return result;
         }
 
-        void CompileMetaPropertyDefinitions(AstMetaProperty ast, MetaProperty mp)
+        internal void CompileMetaPropertyDefinitions(AstMetaProperty ast, MetaProperty mp)
         {
             var defs = new MetaDefinition[ast.Definitions.Count];
 

src/compiler/core/Syntax/Builders/BlockBuilder.FlattenType.cs
@@ -8,7 +8,7 @@ namespace Uno.Compiler.Core.Syntax.Builders
     {
         internal readonly Dictionary<DataType, HashSet<DataType>> FlattenedTypes = new Dictionary<DataType, HashSet<DataType>>();
 
-        void FlattenTypes()
+        internal void FlattenTypes()
         {
             FlattenTypes(_il);
 

src/compiler/core/Syntax/Builders/BlockBuilder.cs
@@ -22,20 +22,19 @@ namespace Uno.Compiler.Core.Syntax.Builders
         readonly ILFactory _ilf;
         readonly NameResolver _resolver;
         readonly Compiler _compiler;
+        readonly BuildQueue _queue;
         readonly Dictionary<AstBlockBase, Block> _processedBlocks = new Dictionary<AstBlockBase, Block>();
-        readonly List<BlockBase> _enqueuedBlocks = new List<BlockBase>();
-        readonly List<KeyValuePair<AstMetaProperty, MetaProperty>> _enqueuedProperties = new List<KeyValuePair<AstMetaProperty, MetaProperty>>();
-        readonly HashSet<DataType> _enqueuedDrawClasses = new HashSet<DataType>();
 
         Block _terminals;
         readonly HashSet<string> _terminalNames = new HashSet<string>();
 
-        public BlockBuilder(
+        internal BlockBuilder(
             Backend backend,
             Namespace il,
             ILFactory ilf,
             NameResolver resolver,
-            Compiler compiler)
+            Compiler compiler,
+            BuildQueue queue)
             : base(compiler)
         {
             _backend = backend;
@@ -43,33 +42,7 @@ namespace Uno.Compiler.Core.Syntax.Builders
             _ilf = ilf;
             _resolver = resolver;
             _compiler = compiler;
-        }
-
-        void EnqueueBlock(BlockBase b, Action<BlockBase> populate)
-        {
-            b.Populating = populate;
-            _enqueuedBlocks.Add(b);
-        }
-
-        public void Build()
-        {
-            FlattenTypes();
-
-            for (int i = 0; i < _enqueuedBlocks.Count; i++)
-                if (!_enqueuedBlocks[i].Source.Bundle.CanLink)
-                    _enqueuedBlocks[i].Populate();
-
-            _enqueuedBlocks.Clear();
-
-            for (int i = 0; i < _enqueuedProperties.Count; i++)
-                CompileMetaPropertyDefinitions(_enqueuedProperties[i].Key, _enqueuedProperties[i].Value);
-
-            _enqueuedProperties.Clear();
-
-            foreach (var dt in _enqueuedDrawClasses)
-                DrawCallGenerator.GenerateDrawCalls(_compiler, dt);
-
-            _enqueuedDrawClasses.Clear();
+            _queue = queue;
         }
 
         public Block TerminalProperties
@@ -83,7 +56,6 @@ namespace Uno.Compiler.Core.Syntax.Builders
 
                     foreach (var t in _backend.ShaderBackend.InputProperties)
                         CompileTerminalProperty(t);
-
                     foreach (var t in _backend.ShaderBackend.OutputProperties)
                         CompileTerminalProperty(t);
                 }

commit a26e5f44b229bc8865574dfd8fce4dc8f1dba1f1

compiler: fix build queue regressions
This fixes five problems experienced on subsequent builds using the
dotnet backend (regressed in 40403e7):

* Resolving an extension method
* Resolving the application constructor in TestTestupTransform
* Resolving sizeof expressions
* Resolving a type nested inside a generic type
* Validating a generic type argument

All problems were already fixed on the lazy branch so the fixes were
cherry-picked together with a few similar fixes.

src/compiler/api/Domain/Graphics/Drawable.cs
@@ -56,6 +56,7 @@ namespace Uno.Compiler.API.Domain.Graphics
                 Block = block;
                 Top = top;
                 Bottom = bottom;
+                Block.Populate();
             }
 
             public Node(Expression obj, BlockBase block)

src/compiler/core/IL/Validation/ILVerifier.Type.cs
@@ -348,6 +348,10 @@ namespace Uno.Compiler.Core.IL.Validation
                             break;
                     }
 
+                    // Skip lazy populated type
+                    if (at.CanLink && at.Constructors.Count == 0)
+                        continue;
+
                     var defCtor = at.TryGetDefaultConstructor();
 
                     if (gt.Constructors.Count > 0 && (

src/compiler/core/Syntax/Binding/NameResolver.cs
@@ -205,6 +205,9 @@ namespace Uno.Compiler.Core.Syntax.Binding
                         var fieldSizeSum = 0;
                         var fieldAlignMax = 1;
 
+                        // Lazy populate fields
+                        dt.PopulateMembers();
+
                         foreach (var f in dt.EnumerateFields())
                         {
                             int fieldSize;

src/compiler/core/Syntax/Builders/TypeBuilder.Parameterize.cs
@@ -241,6 +241,10 @@ namespace Uno.Compiler.Core.Syntax.Builders
                 {
                     bool innerTypeFound = false;
 
+                    // Lazy populate nested types
+                    if (p.NestedTypes.Count == 0)
+                        ParameterizeInnerTypes(p.MasterDefinition, p.MasterDefinition, map, p);
+
                     foreach (var it in p.NestedTypes)
                     {
                         if (arg.MasterDefinition == it.MasterDefinition)

src/compiler/core/Syntax/Compilers/FunctionCompiler.NameResolver.TypeExtension.cs
@@ -46,12 +46,17 @@ namespace Uno.Compiler.Core.Syntax.Compilers
             var extensionMethods = new List<Method>();
 
             foreach (var dt in staticClasses)
+            {
+                // Lazy populate methods
+                dt.PopulateMembers();
+
                 foreach (var m in dt.Methods)
                     if (m.IsStatic &&
                         m.Parameters.Length > 0 && m.Parameters[0].Modifier == ParameterModifier.This &&
                         (typeParamCount == null || m.IsGenericDefinition && m.GenericParameters.Length == typeParamCount) &&
                         m.Name == name)
                         extensionMethods.Add(m);
+            }
 
             if (extensionMethods.Count == 0)
                 return null;

src/compiler/core/Syntax/Generators/DrawCallGenerator.cs
@@ -94,6 +94,8 @@ namespace Uno.Compiler.Core.Syntax.Generators
 
         static void GenerateDrawCalls(Compiler compiler, Method initMethod, Method freeMethod, DrawBlock drawBlock, HashSet<Scope> drawScopes)
         {
+            compiler.Queue.BuildMetaProperties();
+
             foreach (var drawable in drawBlock.Drawables)
             {
                 new ShaderGenerator(compiler, drawable, initMethod.Body, freeMethod.Body, drawBlock.DrawScope).Generate();

src/compiler/core/Syntax/UxlProcessor.cs
@@ -254,6 +254,7 @@ namespace Uno.Compiler.Core.Syntax
                 return;
             }
 
+            dt.AssignAttributes();
             if (!dt.IsIntrinsic &&
                 !dt.HasAttribute(_ilf.Essentials.TargetSpecificImplementationAttribute) &&
                 !dt.HasAttribute(_ilf.Essentials.TargetSpecificTypeAttribute))
@@ -326,6 +327,7 @@ namespace Uno.Compiler.Core.Syntax
             else if (!method.ReturnType.IsVoid)
                 Log.Error(uxl.Signature.Source, ErrorCode.E0000, "Requiring return type for " + method.Quote() + " (" + method.ReturnType + ")");
 
+            method.AssignAttributes();
             if (!method.HasAttribute(_ilf.Essentials.TargetSpecificImplementationAttribute) &&
                 !method.IsIntrinsic)
                 Log.Error(uxl.Signature.Source, ErrorCode.E0000, method.Quote() + " cannot be extended because it does not specify " + _ilf.Essentials.TargetSpecificImplementationAttribute.AttributeString);

src/compiler/core/IL/Testing/TestSetupTransform.cs
@@ -45,6 +45,9 @@ namespace Uno.Compiler.Core.IL.Testing
 
             _appClass = Essentials.Application;
 
+            // Lazy populate app class
+            _appClass.PopulateMembers();
+
             _source = Bundle.Source;
             _testRegistryType = ILFactory.GetType("Uno.Testing.Registry");
             _mainClass = new ClassType(_source, Data.IL, null, Modifiers.Generated | Modifiers.Public, "MainClass");